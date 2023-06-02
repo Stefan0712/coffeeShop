19a02eb05c42d3cfb71dc47246241b90f3fd4d43
Stefan0712 <s.vladulescu@gmail.com>
2023-06-03 02:38:22 +0300

    First commit. Done home page

---
+++ public/js/index.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var testimonials = document.querySelectorAll('.testimonial-body');
+    var currentTestimonial = 0;
+    var nextTestimonial = 1;
+  
+    testimonials[currentTestimonial].style.display = 'flex';
+  
+    function showNextTestimonial() {
+      testimonials[currentTestimonial].style.animation = 'slide-out-right 0.5s forwards';
+      testimonials[nextTestimonial].style.display = 'flex';
+      testimonials[nextTestimonial].style.animation = 'slide-in-left 0.5s forwards';
+  
+      setTimeout(function() {
+        testimonials[currentTestimonial].style.display = 'none';
+        testimonials[currentTestimonial].style.animation = '';
+        currentTestimonial = (currentTestimonial + 1) % testimonials.length;
+        nextTestimonial = (currentTestimonial + 1) % testimonials.length;
+      }, 500);
+    }
+  
+    setInterval(showNextTestimonial, 5000); // Change the interval duration as desired
+  });
+  
